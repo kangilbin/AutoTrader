851acc732012243e3492406e76c618b3c84ae25c
kib3388 <kib3388@naver.com>
2025-03-20 17:11:11 +0900

    스케줄 배치

--- batch/AutoSwingBatch.py
+++ batch/AutoSwingBatch.py
@@ -1,11 +1,14 @@
 from main import app
+from module.AESCrypto import decrypt
 from services.SwingService import get_all_swing
 
 
-async def job():
+async def trade_job():
     swing_list = await get_all_swing(app.state.db_pool)
     for swing in swing_list:
         print("#################스윙 시작################")
-        print(swing.SWING_ID, swing.STOCK_CODE, swing.APP_KEY, swing.SECRET_KEY)
+        print(swing.SWING_ID, swing.STOCK_CODE, decrypt(swing.APP_KEY), decrypt(swing.SECRET_KEY))
 
     print("#################스윙 끝################")
+
+# 일 데이터 수집 (고가, 저가, 종가, 거래량)
