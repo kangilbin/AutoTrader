e4525938f7679a3a6ec029fed7f7acd0a926055a
강일빈 <kib3388@naver.com>
2025-06-18 22:22:31 +0900

    리프레쉬 토큰 검증 추가

--- app/module/Config.py
+++ app/module/Config.py
@@ -7,5 +7,5 @@ load_dotenv(dotenv_path=".env")
 def get_env(key: str) -> str:
     value = os.getenv(key)
     if value is None:
-        raise ValueError(f"Environment variable {key} not set")
+        raise ValueError(f"환경 변수 {key}가 설정되지 않았습니다")
     return value
